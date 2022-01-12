fada8c443f41e60747d9c496f7d69e9179954609
joselito <joselitorebello20@gmail.com>
2022-01-12 23:27:52 +0530

    Web dev practice day 5

--- nodejs_projects/task-manager/controllers/tasks.js
+++ nodejs_projects/task-manager/controllers/tasks.js
@@ -42,8 +42,23 @@ const updateTask = (req, res) => {
   res.send("Update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+const deleteTask = async(req, res) => {
+  try{
+    const {id:taskID} = req.params;
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if (!task){
+      return res.status(404).json({msg: `no Task with id : ${taskID}`})
+    }
+    return res.status(200).json({task})
+
+  }
+  catch(error){
+    res.status(500).json({ error })
+
+  }
+
+  // res.send("Delete task");
+
 };
 
 module.exports = {
