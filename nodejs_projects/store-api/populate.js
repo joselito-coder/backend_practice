ef2df99504f5efd179076f4b731390b9855687aa
joselito <joselitorebello20@gmail.com>
2022-01-31 09:57:42 +0530

    Web dev practice day 10

---
+++ nodejs_projects/store-api/populate.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+
+const connectDB = require('./db/connect');
+const Products = require('./models/product');
+
+const jsonProducts = require('./products.json');
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI);
+        await Products.deleteMany();
+        await Products.create(jsonProducts);
+        console.log("Success")
+        process.exit()
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
