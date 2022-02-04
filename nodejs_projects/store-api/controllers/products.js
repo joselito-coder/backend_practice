f1bf2145145bd37b6273c4b8b6058a893caf7816
joselito <joselitorebello20@gmail.com>
2022-02-04 17:45:16 +0530

    Web dev practice day 13

--- nodejs_projects/store-api/controllers/products.js
+++ nodejs_projects/store-api/controllers/products.js
@@ -2,17 +2,16 @@ const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    const search = 'ab';
+    const search = '';
     const products = await Product.find({
         name: { $regex: search, $options: 'i' }
-    })
+    }).sort('-name price')
 
     res.status(200).json({ products, nBHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    // console.log(req.query)
-    const { featured, company, name } = req.query
+    const { featured, company, name,sort } = req.query
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -26,9 +25,18 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' };
     }
 
-    console.log(queryObject);
+    // console.log(queryObject);
 
-    const products = await Product.find(queryObject);
+    let  result =  Product.find(queryObject);
+    if(sort){
+        // console.log(sort)
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList);
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    const products = await result;
     res.status(200).json({ products, nBHits: products.length })
 }
 
