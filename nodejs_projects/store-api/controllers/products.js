688efa17a78743b7b8ec8f6b240274134b8192ac
joselito <joselitorebello20@gmail.com>
2022-02-06 09:25:09 +0530

    Web dev practice day 14

--- nodejs_projects/store-api/controllers/products.js
+++ nodejs_projects/store-api/controllers/products.js
@@ -5,14 +5,17 @@ const getAllProductsStatic = async (req, res) => {
     const search = '';
     const products = await Product.find({
         name: { $regex: search, $options: 'i' }
-    }).sort('-name price')
+    })
+    .sort('name')
+    .select('name price')
 
     res.status(200).json({ products, nBHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name,sort } = req.query
+    const { featured, company, name, sort,fields } = req.query
     const queryObject = {}
+
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
     }
@@ -25,17 +28,30 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' };
     }
 
-    // console.log(queryObject);
-
-    let  result =  Product.find(queryObject);
-    if(sort){
+    // sort
+    let result = Product.find(queryObject);
+    if (sort) {
         // console.log(sort)
         const sortList = sort.split(',').join(' ')
         result = result.sort(sortList);
     }
-    else{
+    else {
         result = result.sort('createdAt')
     }
+
+
+    if(fields){
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit= Number(req.query.limit) || 10
+    const skip = (page -1 ) * limit
+
+    result = result.skip(skip).limit(limit);
+
+
     const products = await result;
     res.status(200).json({ products, nBHits: products.length })
 }
