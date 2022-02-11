6bd8f757061b9cca72504c183a8dbcd341552025
joselito <joselitorebello20@gmail.com>
2022-02-11 11:02:41 +0530

    Web dev practice day 19

---
+++ nodejs_projects/jobs-api/controllers/jobs.js
@@ -0,0 +1,29 @@
+
+const getAllJobs = (req, res) => {
+    res.send("Get all Jobs")
+}
+
+const getJob = (req, res) => {
+    res.send("Get single job")
+}
+
+const updateJob = (req, res) => {
+    res.send("Update job")
+}
+
+const createJob = (req, res) => {
+    res.send("Create job")
+}
+
+const deleteJob = (req, res) => {
+    res.send("delete job")
+}
+
+module.exports = {
+    createJob,
+    deleteJob,
+    updateJob,
+    getAllJobs,
+    getJob,
+
+}
