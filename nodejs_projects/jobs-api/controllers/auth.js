700c91744bf647a33fa23f9a6f800633daec6db1
joselito <joselitorebello20@gmail.com>
2022-02-15 10:23:01 +0530

    Web dev practice day 22

--- nodejs_projects/jobs-api/controllers/auth.js
+++ nodejs_projects/jobs-api/controllers/auth.js
@@ -1,32 +1,37 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError,UnauthenticatedError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const nukeDB = async (req,res) =>{
-    const {secret} = req.body;
-    const mySecret = process.env.SEKRET;
-
-    if(!secret){
-        throw new BadRequestError("LOl Chomu")
-    }
-    else if(secret != mySecret){
-        throw new UnauthenticatedError("LOL Get rick rolled")
-    }
-
-    const user = await User.deleteMany({});
-    return res.status(200).json({msg:"LOL Chomu the nuke was a success",count:user.deletedCount});
-
-}
 const register = async (req, res) => {
 
     const user = await User.create({ ...req.body })
 
-    res.status(StatusCodes.CREATED).json(user)
+    const token = user.createJWT();
+
+    res.status(StatusCodes.CREATED).json({ user:{name: user.getName(), },token })
 }
 
 const login = (req, res) => {
     res.send("login user")
+}
+
+
+const nukeDB = async (req, res) => {
+    const { secret } = req.body;
+    const mySecret = process.env.SEKRET;
+
+    if (!secret) {
+        throw new BadRequestError("LOl Chomu")
+    }
+    else if (secret != mySecret) {
+        throw new UnauthenticatedError("LOL Get rick rolled")
+    }
+
+    const user = await User.deleteMany({});
+    return res.status(200).json({ msg: "LOL Chomu the nuke was a success", count: user.deletedCount });
+
 }
 
 module.exports = { register, login, nukeDB }
--- nodejs_projects/jobs-api/models/User.js
+++ nodejs_projects/jobs-api/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 const UserSchema = new mongoose.Schema({
@@ -17,7 +18,7 @@ const UserSchema = new mongoose.Schema({
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please provide valid email'
         ],
-        unique:true,
+        unique: true,
 
     },
 
@@ -29,10 +30,18 @@ const UserSchema = new mongoose.Schema({
 
 })
 
-UserSchema.pre('save', async function(next){
+UserSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password,salt);
-    next();
+    this.password = await bcrypt.hash(this.password, salt);
 })
 
-module.exports = mongoose.model('User',UserSchema)
+UserSchema.methods.getName = function () {
+    return this.name;
+}
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
+}
+
+
+module.exports = mongoose.model('User', UserSchema)
