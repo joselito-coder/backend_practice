33ef780e15052ac4197147287d03b7f639f8282f
joselito <joselitorebello20@gmail.com>
2022-02-23 07:05:04 +0530

    Web dev practice day 29

--- nodejs_projects/jobs-api/app.js
+++ nodejs_projects/jobs-api/app.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
 // Database
 const connectDB = require('./db/connect')
 
 // import Middlewares
 const authenticateUser = require('./middleware/authentication');
+
+// Security
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 
 
 // routers
@@ -18,8 +23,25 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// use middleware
 app.use(express.json());
-// extra packages
+
+app.set('trust proxy',1);
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+)
+
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 // routes
 app.use('/api/v1/auth', authRouter)
